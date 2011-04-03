a01d507c5d1eda598a0cd193a5c990e0595e907e
Thiago Caiubi <thiago.caiubi@gmail.com>
2011-04-03 14:46:09 -0300

    Improve help

--- public/js/help.js
+++ public/js/help.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
 	$.facebox.settings.closeImage = '/lib/facebox/closelabel.png';
 	$.facebox.settings.loadingImage = '/lib/facebox/loading.gif';
+	
+	$(document).bind('afterReveal.facebox', function(){
+		$('#info .play').click(function(){
+			$(document).trigger('close.facebox');
+		});
+	});
+	
+	$.facebox({div: '#help'});
+	
 	$('#help').click(function(event){
 		$.facebox({div: '#' + event.target.id});
-	}).click();
+	});
 });
